38b0541d1eeabf967bd3fca6fabe0125b59beb93
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2022-04-10 11:51:45 +0200

    code clean

--- include/sc_geometry.h
+++ include/sc_geometry.h
@@ -1,17 +1,7 @@
 #ifndef _SC_GEOMETRY_H
 #define _SC_GEOMETRY_H
 
-struct sc_point {
-	double x;
-	double y;
-};
 
-struct sc_rect {
-	double x;
-	double y;
-	double width;
-	double height;
-};
 
 #endif
 
